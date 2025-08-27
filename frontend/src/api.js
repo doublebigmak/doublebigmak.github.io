a6e9864f6b801e734822e024c6896c495ade05f1
Michael Mak <mak.michael.609@gmail.com>
2025-08-27 15:38:19 -0700

    api changes

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,5 +1,5 @@
 
-const API_URL = process.env.REACT_APP_API_URL || '';
+const API_URL = process.env.REACT_APP_API_URL || 'http://3.135.62.56/';
 
 export async function fetchTodayGames() {
   const response = await fetch(`${API_URL}/game/today`);
